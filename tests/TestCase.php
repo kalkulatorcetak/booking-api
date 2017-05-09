e45ed04b8e0fa1936af79782b3f945425275f535
ngabor84 <negabor@gmail.com>
2017-05-09 17:44:06 +0200

    add token for testcase setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,6 @@ use Lukasoppermann\Httpstatus\Httpstatuscodes;
 abstract class TestCase extends BaseTestCase implements Httpstatuscodes
 {
     use TestTrait;
-
     protected $client;
 
     public function setUp()
@@ -20,7 +19,8 @@ abstract class TestCase extends BaseTestCase implements Httpstatuscodes
             'base_uri' => 'http://booking-api.dev',
             'exceptions' => false,
             'headers' => [
-                'Accept' => 'application/vnd.booking.v1+json'
+                'Accept' => 'application/vnd.booking.v1+json',
+                'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYm9va2luZy1hcGkuZGV2L2F1dGgvbG9naW4iLCJpYXQiOjE0OTQyMjIwODgsImV4cCI6MTQ5Njg1MDA4OCwibmJmIjoxNDk0MjIyMDg4LCJqdGkiOiJUejBhU2VJakNKSU8zZE51Iiwic3ViIjoxfQ.KTmaDcbxeNVmbBwBPIQOD4fl65UNaLMlo2oksmHFlYA',
             ]
         ]);
     }
