0cd3d5d0732d0ea0b3c724e78497e1ba00e2e66a
ngabor84 <negabor@gmail.com>
2017-05-24 18:23:14 +0200

    add new env variable for codeship build url

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,7 +25,7 @@ abstract class TestCase extends BaseTestCase implements Httpstatuscodes
         $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vOiIsImlhdCI6MTQ5NTA4ODg2OSwiZXhwIjoxNDk3NzE2ODY5LCJuYmYiOjE0OTUwODg4NjksImp0aSI6IjVteDJQOXRiMjFhdmFsS0wiLCJzdWIiOjF9.kutMfu0YrojY_JafRPVk1pVNVVddQgDaA1mp1N0GYG8';
 
         $this->client = new Client([
-            'base_uri' => 'http://test.booking-api.dev',
+            'base_uri' => env('CI_BUILD_URL', 'http://test.booking-api.dev'),
             'exceptions' => false,
             'headers' => [
                 'Accept' => 'application/vnd.booking.v1+json',
