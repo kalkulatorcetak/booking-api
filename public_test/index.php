e4cbaa513902471762b4c740db487648616c85d8
ngabor84 <negabor@gmail.com>
2017-05-24 18:29:53 +0200

    add new env variable for codeship build url

--- public_test/index.php
+++ public_test/index.php
@@ -12,8 +12,11 @@
 */
 putenv('APP_ENV=testing');
 putenv('DB_CONNECTION=testing');
-putenv('API_DOMAIN=test.booking-api.dev');
-
+if (env('CI_BUILD_URL')) {
+    putenv('API_DOMAIN=' . env('CI_BUILD_URL'));
+} else {
+    putenv('API_DOMAIN=test.booking-api.dev');
+}
 $app = require __DIR__ . '/../bootstrap/app.php';
 
 /*
